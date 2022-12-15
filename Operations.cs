14c8e94c4eebf9a932de3906ab62a017e2033216
RahulAid <raccess97@gmail.com>
2022-12-15 23:35:55 +0530

    Check if File Exists

---
+++ Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperations
+{
+    public class Operations
+    {
+        public void FileExists()
+        {
+            string filePath = @"D:\BridgeLabzz\FileIOOperations\WordFile1.txt";
+            
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("\nFile Exists");
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
